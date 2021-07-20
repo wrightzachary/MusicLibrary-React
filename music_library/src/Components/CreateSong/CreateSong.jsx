ea603e96019249a71bdfac6361436d0a93702744
Andy Wade <carrollandywadejr@gmail.com>
2021-07-20 13:31:15 -0500

    added styling

--- music_library/src/Components/CreateSong/CreateSong.jsx
+++ music_library/src/Components/CreateSong/CreateSong.jsx
@@ -18,7 +18,6 @@ class CreateSong extends React.Component {
     }
   
     handleChange(event) {
-      console.log("event",event)
         this.setState({
             [event.target.name]: event.target.value
         });
--- music_library/src/Components/FilterSearch/FilterSearch.jsx
+++ music_library/src/Components/FilterSearch/FilterSearch.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './FilterSearch';
 
 class FilterSearch extends Component{
     constructor(props) {
